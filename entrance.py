9f1b440dd19ad81e9414840ed3a0920c28b6eedf
Matrix <Matrix@caiex.com>
2017-08-16 19:17:32 +0800

    add instruction

--- entrance.py
+++ entrance.py
@@ -6,22 +6,35 @@ import sys
 
 TYPE = ['score', 'inplay', 'early']
 
+
+def print_usage():
+    print 'You should specify checking keyword: {score, inplay, early}'
+    print 'python entrance.py score ${europe_id}'
+    print 'python entrance.py inplay ${europe_id} ${company_id}'
+    print 'python entrance.py early ${europe_id} ${company_id}'
+
+
 if __name__ == '__main__':
     ARGS = sys.argv
-    type = ARGS[1]
-    if type not in TYPE:
-        print 'You should specify checking keyword: {score, inplay, early}'
+
+    if len(ARGS) < 2 or ARGS[1] not in TYPE:
+        print_usage()
+
     europe_id = ARGS[2]
-    if type != 'score':
-        company_id = ARGS[3]
+
     p = None
-    if type == 'score':
+
+    check_type = ARGS[1]
+
+    if check_type == 'score':
         d = downloader.ScoreDownloader()
         p = parser.ScoreParser(d.download(), europe_id)
-    elif type == 'inplay':
+    elif check_type == 'inplay':
         d = downloader.InplayOddsDownloader()
+        company_id = ARGS[3]
         p = parser.InplayParser(d.download(), europe_id, company_id)
-    elif type == 'early':
+    elif check_type == 'early':
+        company_id = ARGS[3]
         d = downloader.EarlyOddsDownloader()
         p = parser.EarlyOddsParser(d.download(), europe_id, company_id)
 

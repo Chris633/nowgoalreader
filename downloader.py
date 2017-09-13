ab495048b305c1fde01f21e0c5460d95787afb00
chris633 <378151018@qq.com>
2017-09-13 15:15:30 +0800

    init new branch for basketball

--- downloader.py
+++ downloader.py
@@ -1,9 +1,6 @@
-#-*- coding:utf8 -*-
+# -*- coding:utf8 -*-
 import urllib2
-import sys
-default_encoding = 'utf-8'
-reload(sys)
-sys.setdefaultencoding(default_encoding)
+
 
 class Downloader:
 
@@ -20,18 +17,8 @@ class EarlyOddsDownloader(Downloader):
         Downloader.__init__(self)
 
     def download(self):
-        response = urllib2.urlopen('http://interface.win007.com/zq/odds.aspx')
-        # response = open('odds.txt', 'r')
-        return response.read()
-
-
-class InplayOddsDownloader(Downloader):
-    def __init__(self):
-        Downloader.__init__(self)
-
-    def download(self):
-        response = urllib2.urlopen('http://interface.win007.com/zq/Odds_Running.aspx')
-        # response = open('team.xml', 'r')
+        response = urllib2.urlopen('http://interface.win007.com/lq/LqOdds.aspx')
+        # response = open('LqOdds.aspx', 'r')
         return response.read()
 
 
@@ -40,6 +27,6 @@ class ScoreDownloader(Downloader):
         Downloader.__init__(self)
 
     def download(self):
-        response = urllib2.urlopen('http://interface.win007.com/zq/today.aspx')
-        # response = open('score.xml', 'r')
+        response = urllib2.urlopen('http://interface.win007.com/lq/today.aspx')
+        # response = open('today.aspx', 'r')
         return response.read()
--- entrance.py
+++ entrance.py
@@ -1,27 +1,37 @@
 # -*- coding:utf8 -*-
-
 import downloader
 import parser
 import sys
+default_encoding = 'utf-8'
+reload(sys)
+sys.setdefaultencoding(default_encoding)
+TYPE = ['score', 'early']
 
-TYPE = ['score', 'inplay', 'early']
+
+def print_usage():
+    print 'You should specify checking keyword: {score, inplay, early}'
+    print 'python entrance.py score ${europe_id}'
+    print 'python entrance.py early ${europe_id} ${company_id}'
+
 
 if __name__ == '__main__':
     ARGS = sys.argv
-    type = ARGS[1]
-    if type not in TYPE:
-        print 'You should specify checking keyword: {score, inplay, early}'
+
+    if len(ARGS) < 3 or ARGS[1] not in TYPE:
+        print_usage()
+        sys.exit()
+
     europe_id = ARGS[2]
-    if type != 'score':
-        company_id = ARGS[3]
+
     p = None
-    if type == 'score':
+
+    check_type = ARGS[1]
+
+    if check_type == 'score':
         d = downloader.ScoreDownloader()
         p = parser.ScoreParser(d.download(), europe_id)
-    elif type == 'inplay':
-        d = downloader.InplayOddsDownloader()
-        p = parser.InplayParser(d.download(), europe_id, company_id)
-    elif type == 'early':
+    elif check_type == 'early':
+        company_id = ARGS[3]
         d = downloader.EarlyOddsDownloader()
         p = parser.EarlyOddsParser(d.download(), europe_id, company_id)
 
